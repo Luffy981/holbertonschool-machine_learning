67d2420ef1d3e138be5ef04e6c2767b4a52334a6
luffy <jhonsmith981@hotmail.com>
2022-04-11 14:26:54 -0500

    shape matrix

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def matrix_shape(matrix):
+    try:
+        if matrix[0][0][0]:
+            return [len(matrix), len(matrix[0]), len(matrix[0][0])]
+    except Exception:
+        pass
+    return [len(matrix), len(matrix[0])]
