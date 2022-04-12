a589fbac06227b7362676ea766aa4964063afcbd
luffy <jhonsmith981@hotmail.com>
2022-04-12 00:14:24 -0500

    concat matrix

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
-import copy
+def mydeepcopy(L):
+    if isinstance(L, list):
+        ret = []
+        for i in L:
+            ret.append(mydeepcopy(i))
+    elif isinstance(L, (int, float, type(None), str, bool)):
+        ret = L
+    else:
+        raise ValueError("Unexpected type for mydeepcopy function")
+
+    return ret
 def cat_matrices2D(mat1, mat2, axis=0):
     """
     concatenates two matrices along a specific axis
     """
-    mat1_copy = copy.deepcopy(mat1)
-    mat2_copy = copy.deepcopy(mat2)
+    mat1_copy = mydeepcopy(mat1)
+    mat2_copy = mydeepcopy(mat2)
     if axis == 0:
         for row in mat2_copy:
             mat1_copy = mat1_copy + [row]
