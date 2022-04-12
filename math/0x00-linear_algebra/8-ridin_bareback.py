bd82c504f7d4ab0f72d76a87feff223baf5d1c9f
luffy <jhonsmith981@hotmail.com>
2022-04-12 00:35:21 -0500

    multiply matrix

---
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def mat_mul(mat1, mat2):
+    """
+    performs matrix multiplication
+    """
+    mul = []
+    for i in range(len(mat1)):
+        lit = []
+        for k in range(len(mat2[0])):
+            sum = 0
+            for j in range(len(mat2)):
+                sum +=  mat1[i][j] * mat2[j][k]
+            lit.append(sum)
+        mul.append(lit)
+    return mul
