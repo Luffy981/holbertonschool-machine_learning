4c11782308cb7ebf48144ddaecc312c30fa5145c
luffy <jhonsmith981@hotmail.com>
2022-04-13 16:12:00 -0500

    shape matrix

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
+    """
+    size matrix
+    """
     try:
         if matrix[0][0][0]:
             return [len(matrix), len(matrix[0]), len(matrix[0][0])]
