87f70aae3617c4180248206a20d58eeff9d9356d
luffy <jhonsmith981@hotmail.com>
2022-04-13 16:21:08 -0500

    concatenate matrix

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env  python3
+"""concat matrix"""
+
+
 def cat_arrays(arr1, arr2):
+    """concat matrix"""
     arr1_copy = arr1[:]
     arr2_copy = arr2[:]
     new_array = arr1_copy + arr2_copy
