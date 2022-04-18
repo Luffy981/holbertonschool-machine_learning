9d15436abc67bfc92bc9cc7146ffef4afa16c7a4
luffy <jhonsmith981@hotmail.com>
2022-04-18 13:32:03 -0500

    line pyplot

--- math/0x01-plotting/1-scatter.py
+++ math/0x01-plotting/1-scatter.py
@@ -8,7 +8,7 @@ np.random.seed(5)
 x, y = np.random.multivariate_normal(mean, cov, 2000).T
 y += 180
 
-plt.scatter(x, y, c='m')
+plt.scatter(x, y, color='magenta')
 plt.xlabel('Height (in)')
 plt.ylabel('Weight (lbs)')
 plt.title('Men\'s Height vs Weigh')
