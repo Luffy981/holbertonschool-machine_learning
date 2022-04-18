64c70ee11775e0fe9495bc8633b730e99834c6dc
luffy <jhonsmith981@hotmail.com>
2022-04-18 18:05:25 -0500

    histogram with pyplot

---
+++ math/0x01-plotting/4-frequency.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+plt.hist(student_grades, bins=10, edgecolor = "black")
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.title('Project A')
+plt.xlim([0, 100])
+plt.show()
