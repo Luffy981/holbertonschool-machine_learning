c79cb408662e6715bf0af941d6e3ed09bbcd43da
luffy <jhonsmith981@hotmail.com>
2022-04-19 01:18:58 -0500

    bars pyplot

--- math/0x01-plotting/6-bars.py
+++ math/0x01-plotting/6-bars.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 np.random.seed(5)
-fruit = np.random.randint(0, 20, (4,3))
+fruit = np.random.randint(0, 20, (4, 3))
 
 columns = ['Farrah', 'Fred', 'Felicia']
 apples = fruit[0]
@@ -13,9 +13,12 @@ peaches = fruit[3]
 width = 0.5
 fig, ax = plt.subplots()
 ax.bar(columns, apples, width, label='apples', color='red')
-ax.bar(columns, bananas, width, bottom=fruit[0], label='bananas', color='yellow')
-ax.bar(columns, oranges, width, bottom=fruit[1] + fruit[0], label='oranges', color='#ff8000')
-ax.bar(columns, peaches, width, bottom=fruit[2] + fruit[1] + fruit[0], label='peaches', color='#ffe5b4')
+ax.bar(columns, bananas, width, bottom=fruit[0],
+       label='bananas', color='yellow')
+ax.bar(columns, oranges, width, bottom=fruit[1] + fruit[0],
+       label='oranges', color='#ff8000')
+ax.bar(columns, peaches, width, bottom=fruit[2] + fruit[1] + fruit[0],
+       label='peaches', color='#ffe5b4')
 ax.set_ylabel('Quantity of Fruit')
 ax.set_ylim([0, 80])
 ax.set_title('Number of Fruit per Person')
