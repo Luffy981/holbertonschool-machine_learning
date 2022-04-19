4136ca6c1c4a86e2f217e77749f7bffadd296471
luffy <jhonsmith981@hotmail.com>
2022-04-19 01:18:40 -0500

    hist pyplot

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
-plt.hist(student_grades, bins=10, edgecolor = "black")
+plt.hist(student_grades, bins=10, edgecolor='black')
 plt.xlabel('Grades')
 plt.ylabel('Number of Students')
 plt.title('Project A')
