7dcbac95461ecdbbdcd5696954da010de933595f
luffy <jhonsmith981@hotmail.com>
2022-04-21 23:09:35 -0500

    matisse

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -3,9 +3,13 @@
 
 
 def poly_derivative(poly):
-    """Derivates"""
+    """Returns the derivative of the given poly """
+    if type(poly) is not list or len(poly) == 0:
+        return None
     derivate = []
     for i in range(len(poly)):
         if i >= 1:
             derivate.append(poly[i] * i)
+    if len(derivate) == 0:
+        derivate.append(0)
     return derivate
