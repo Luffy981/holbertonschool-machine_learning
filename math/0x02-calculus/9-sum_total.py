68caf5b81b790d967de169dcdb33dba5df543843
luffy <jhonsmith981@hotmail.com>
2022-04-21 23:13:25 -0500

    sum

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n):
     """sum squared"""
-    if type(n) != int and n <= 0:
+    if type(n) != int and n <= 0 and n is None:
         return None
-    return (n*(n + 1)*(2*n + 1))/6
+    return (n * (n + 1) * (2 * n + 1)) / 6
