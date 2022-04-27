5dd5321c3b6f5cc0b258f15806161d9fa57e94d6
luffy <jhonsmith981@hotmail.com>
2022-04-27 15:21:10 -0500

    normal distribution

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Normal class"""
+
+
+class Normal:
+    """Normal distribution"""
+    def __init__(self, data=None, mean=0., stddev=1.):
+        self.mean = float(mean)
+        self.stddev = float(stddev)
+        if data is None:
+            if self.stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+        else:
+            if type(data) != list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = sum(data) / len(data)
+            suma = 0
+            for x in data:
+                suma += (x - self.mean) ** 2
+            self.stddev = ((1/len(data)) * suma) ** 1/2
