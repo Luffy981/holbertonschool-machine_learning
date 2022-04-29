c19055ffe52bb921417b72d332a0a6820f12a7e5
luffy <jhonsmith981@hotmail.com>
2022-04-29 11:09:41 -0500

    poisson distribution

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -2,34 +2,33 @@
 """class poisson"""
 
 
+def factorial(number):
+    """Method to calculate Factorial"""
+    fact = 1
+    for num in range(2, number + 1):
+        fact = fact * num
+    return fact
+
+
 class Poisson:
     """Poisson distribution"""
     def __init__(self, data=None, lambtha=1.):
         """Constructor to poisson"""
-        self.lambtha = float(lambtha)
         self.e = 2.7182818285
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
         else:
             if type(data) != list:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            sum_P = 0
+            count = 0
             prob = []
             for x in data:
-                factor = (self.lambtha ** x)
-                sum_P += (self.e**(-self.lambtha) * factor) / factorial(x)
-                prob.append(sum_P)
-            self.lambtha = round(sum_P, 2)
-
-    def factorial(number):
-        """Method to calculate Factorial"""
-        fact = 1
-        for num in range(2, number + 1):
-            fact = fact * num
-        return fact
+                count += x
+            self.lambtha = float(count / len(data))
 
     def pmf(self, k):
         """probability mass function"""
@@ -39,3 +38,15 @@ class Poisson:
             return 0
         PMF = (self.e**(-self.lambtha) * (self.lambtha ** k)) / factorial(k)
         return PMF
+
+    def cdf(self, k):
+        """Cumulative density function"""
+        try:
+            k = int(k)
+        except Exception:
+            return 0
+        CDF = 0
+        for i in range(k + 1):
+            factor = (self.e ** (-self.lambtha) * (self.lambtha ** i))
+            CDF += factor / factorial(i)
+        return CDF
