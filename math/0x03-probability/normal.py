de5b873953e2478d79979ccb36c6ceb8ae6690ec
luffy <jhonsmith981@hotmail.com>
2022-04-30 00:03:25 -0500

    normal distribution

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -2,12 +2,32 @@
 """Normal class"""
 
 
+pi = 3.1415926536
+
+
+def factorial(number):
+    """Method to calculate Factorial"""
+    fact = 1
+    for num in range(2, number + 1):
+        fact = fact * num
+    return fact
+
+
+def erf(t):
+    """Error function"""
+    sum = t - (t ** 3) / 3 + (t ** 5) / 10 - (t ** 7) / 42 + (t ** 9) / 216
+    ERFC = (2 / (pi ** (0.5))) * sum
+    return ERFC
+
+
 class Normal:
     """Normal distribution"""
     def __init__(self, data=None, mean=0., stddev=1.):
+        self.mean = float(mean)
+        self.stddev = float(stddev)
+        self.pi = float(3.1415926536)
+        self.e = float(2.7182818285)
         if data is None:
-            self.mean = float(mean)
-            self.stddev = float(stddev)
             if self.stddev <= 0:
                 raise ValueError("stddev must be a positive value")
         else:
@@ -19,7 +39,7 @@ class Normal:
             suma = 0
             for x in data:
                 suma += (x - self.mean) ** 2
-            self.stddev = (1 / (len(data)) * suma) ** 0.5
+            self.stddev = ((1/len(data)) * suma) ** 1/2
 
     def z_score(self, x):
         """Z-score given x-value"""
@@ -33,7 +53,13 @@ class Normal:
 
     def pdf(self, x):
         """Probability density function"""
-        factor = 1 / (self.stddev * (2 * self.pi) ** 0.5)
+        factor = 1 / (self.stddev * (2 * self.pi) ** 1/2)
         poew = (-1/2 * ((x - self.mean) / self.stddev) ** 2)
         pdf = factor * self.e ** poew
         return pdf
+
+    def cdf(self, x):
+        """Cumulative density function"""
+        factor = (x - self.mean) / (self.stddev * (2 ** 0.5))
+        CDF = (1 / 2) * (1 + erf(factor))
+        return CDF
