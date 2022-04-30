4e61c01cb83647aad9bab64c2a2be41cb3ae9a56
luffy <jhonsmith981@hotmail.com>
2022-04-29 22:14:15 -0500

    binomial distribution

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -5,22 +5,28 @@
 class Binomial:
     """Binomial distribution"""
     def __init__(self, data=None, n=1, p=0.5):
-        self.p = float(p)
-        self.n = float(n)
         if data is None:
             if n < 0:
                 raise ValueError("n must be a positive value")
             if p < 0 or p > 1:
                 raise ValueError("p must be greater than 0 and less than 1")
+            self.p = float(p)
+            self.n = round(n)
         else:
             if type(data) != list:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-
-    def factorial(number):
-        """Method to calculate Factorial"""
-        fact = 1
-        for num in range(2, number + 1):
-            fact = fact * num
-        return fact
+            # mean = np
+            # var = np(1-p)
+            mean = sum(data) / len(data)
+            suma = 0
+            for x in data:
+                suma += (x - mean) ** 2
+            var = (1 / len(data)) * suma
+            # np = mean
+            # np(1 - p) = var
+            # 1 - p = var / mean
+            p = 1 - (var / mean)
+            self.n = round(mean / p)
+            self.p = mean / self.n
