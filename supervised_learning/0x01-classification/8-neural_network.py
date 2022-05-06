d69735000b47b3e1ce9637cb97781c4071492d6a
luffy <jhonsmith981@hotmail.com>
2022-05-06 17:32:08 -0500

    initialize neural network

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 class NeuralNetwork:
     """making Neural network"""
     def __init__(self, nx, nodes):
+        """Initialize neural network"""
         if type(nx) != int:
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -17,9 +18,15 @@ class NeuralNetwork:
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
+        # The weights vector for the hidden layer
         self.W1 = np.random.normal(size=(nodes, nx))
+        # The bias for the hidden layer
         self.b1 = 0
+        # The activated output for the hidden layer
         self.A1 = 0
+        # The weights vector for the output neuron
         self.W2 = np.random.normal(size=(1, nodes))
+        # The bias for the output neuron
         self.b2 = 0
+        # The activated output for the output neuron (prediction)
         self.A2 = 0
