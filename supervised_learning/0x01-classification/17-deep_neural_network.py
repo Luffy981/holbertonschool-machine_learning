fae5b649c9eacc661d577ed60d78b74b50f6448b
luffy <jhonsmith981@hotmail.com>
2022-05-08 16:23:28 -0500

    privatice attributes od deep

---
+++ supervised_learning/0x01-classification/17-deep_neural_network.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""deep neural network"""
+
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """making a deep neural network"""
+    def __init__(self, nx, layers):
+        """initialiaze deep neural network"""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) != list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        """ Using filter to check if any element is negative """
+        negative = list(filter(lambda x: x <= 0, layers))
+        if len(negative) > 0:
+            raise TypeError("layers must be a list of positive integers")
+        """The number of layers in neural network"""
+        self.L = len(layers)
+        """A dictionary to hold all weigths and biased of the network"""
+        self.cache = {}
+        self.weights = {}
+        for i in range(len(layers)):
+            """biases of the network should be initialized to 0’s"""
+            if i == 0:
+                factor1 = np.random.randn(layers[i], nx)
+                factor2 = np.sqrt(2 / nx)
+                self.weights['W' + str(i + 1)] = factor1 * factor2
+            else:
+                """He et a"""
+                factor1 = np.random.randn(layers[i], layers[i - 1])
+                factor2 = np.sqrt(2 / layers[i - 1])
+                self.weights['W' + str(i + 1)] = factor1 * factor2
+            zeros = np.zeros(layers[i])
+            self.weights['b' + str(i + 1)] = zeros.reshape(layers[i], 1)
+
+        @property
+        def L(self):
+            """The number of layers in the neural network."""
+            return self.__L
+
+        @property
+        def cache(self):
+            """ A dictionary to hold all intermediary values of the network."""
+            return self.__cache
+
+        @property
+        def weights(self):
+            """ A dictionary to hold all weights and biased of the network."""
+            return self.__weights
