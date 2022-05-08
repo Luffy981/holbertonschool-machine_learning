e63d2f8de62079c4fe7a0091bfb00b13af7ff276
luffy <jhonsmith981@hotmail.com>
2022-05-08 16:17:48 -0500

    initialize deep neural network

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -13,10 +13,8 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(layers) != list:
+        if type(layers) != list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-
-        """Layers must be a list of positive integers"""
         """ Using filter to check if any element is negative """
         negative = list(filter(lambda x: x <= 0, layers))
         if len(negative) > 0:
