328c30554aef227a9749296a079f3a42f759ab54
luffy <jhonsmith981@hotmail.com>
2022-05-09 14:14:29 -0500

    one hot encode

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -2,12 +2,17 @@
 """One hot encoding"""
 
 
-from sklearn.preprocessing import OneHotEncoder
+import numpy as np
 
 
 def one_hot_encode(Y, classes):
     """Converts a numeric label vector into a one-hot matrix"""
-    data = np.array(Y).reshape(classes, 1)
-    encoder = OneHotEncoder(sparse=False)
-    one_hot = encoder.fit_transform(data)
+    if type(Y) is not np.ndarray:
+        return None
+    if type(classes) is not int:
+        return None
+    try:
+        one_hot = np.eye(classes)[Y]
+    except Exception:
+        return None
     return one_hot
