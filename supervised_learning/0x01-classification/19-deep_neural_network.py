7fae1107abca33d71faf4fae9ba741bc9a8960a1
luffy <jhonsmith981@hotmail.com>
2022-05-09 02:46:08 -0500

    cost deep neural network

---
+++ supervised_learning/0x01-classification/19-deep_neural_network.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""deep neural network"""
+
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """making a deep neural network"""
+    def __init__(self, nx, layers):
+        """initialiaze deep neural network"""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) != list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        """ Using filter to check if any element is negative """
+        negative = list(filter(lambda x: x <= 0, layers))
+        if len(negative) > 0:
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for i in range(len(layers)):
+            """biases of the network should be initialized to 0’s"""
+            if i == 0:
+                factor1 = np.random.randn(layers[i], nx)
+                factor2 = np.sqrt(2 / nx)
+                self.__weights['W' + str(i + 1)] = factor1 * factor2
+            else:
+                """He et a"""
+                factor1 = np.random.randn(layers[i], layers[i - 1])
+                factor2 = np.sqrt(2 / layers[i - 1])
+                self.__weights['W' + str(i + 1)] = factor1 * factor2
+            zeros = np.zeros(layers[i])
+            self.__weights['b' + str(i + 1)] = zeros.reshape(layers[i], 1)
+
+    @property
+    def cache(self):
+        """ A dictionary to hold all intermediary values of the network"""
+        return self.__cache
+
+    @property
+    def L(self):
+        """The number of layers in the neural network"""
+        return self.__L
+
+    @property
+    def weights(self):
+        """ A dictionary to hold all weights and biased of the network"""
+        return self.__weights
+
+    def forward_prop(self, X):
+        """Forward propagation"""
+        self.__cache['A0'] = X
+        for i in range(self.__L):
+            w_layer = self.__weights['W' + str(i + 1)]
+            b_layer = self.__weights['b' + str(i + 1)]
+            activation = sigmoid(np.dot(w_layer, X) + b_layer)
+            X = activation
+            self.__cache['A' + str(i + 1)] = activation
+        return self.__cache['A{}'.format(self.__L)], self.__cache
+
+    def cost(self, Y, A):
+        """Cost or error function"""
+        label_1 = Y * log(A)
+        label_2 = (Y - 1) * log(1.0000001 - A)
+        cost = label_1 * label_2
+        return cost
+
+
+def sigmoid(x):
+    """Sigmoid function"""
+    return 1 / (1 + np.exp(-x))
