191ab2a7a7ca9401c5d53933d9feaec652a149f1
luffy <jhonsmith981@hotmail.com>
2022-05-12 22:57:36 -0500

    evaluate neural network

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Evaluate neural network"""
+
+
+import tensorflow.compat.v1 as tf
+
+
+def evaluate(X, Y, save_path):
+    """Evaluation for classification model"""
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(save_path + '.meta')
+        saver.restore(sess, save_path)
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        y_pred = tf.get_collection('y_pred')[0]
+        loss = tf.get_collection('loss')[0]
+        acc = tf.get_collection('accuracy')[0]
+        vars = sess.run([y_pred, acc, loss], feed_dict={x: X, y: Y})
+        return vars[0], vars[1], vars[2]
