489a3ce6d5952be37b2e7e75c6190b3eb9f6646f
luffy <jhonsmith981@hotmail.com>
2022-05-12 21:37:30 -0500

    Creating layers to neural network

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Create a placeholders"""
 
+
 import tensorflow.compat.v1 as tf
+
 
 def create_layer(prev, n, activation):
     """Create Layer"""
@@ -11,7 +13,7 @@ def create_layer(prev, n, activation):
     # n: is the number of nodes in the layer to create layer
     # Initializer function for the weight matrix
     layers = tf.keras.layers.Dense(activation=activation,
-                             units=n,
-                             kernel_initializer=weights,
-                             name='layer')
+                                   units=n,
+                                   kernel_initializer=weights,
+                                   name='layer')
     return layers(inputs=prev)
