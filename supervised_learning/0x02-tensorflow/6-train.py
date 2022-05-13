10ef5d8162799a7ecf9cb631c75a27be3c8b25b9
luffy <jhonsmith981@hotmail.com>
2022-05-12 23:00:16 -0500

    evaluate neural network

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
-"""Train model"""
+"""Train classification model"""
 
 
-import tensorflow.compat.v1 as tf
-calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
-calculate_loss = __import__('4-calculate_loss').calculate_loss
+import tensorflow as tf
+
+
 create_placeholders = __import__('0-create_placeholders').create_placeholders
-create_train_op = __import__('5-create_train_op').create_train_op
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 forward_prop = __import__('2-forward_prop').forward_prop
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_train_op = __import__('5-create_train_op').create_train_op
 
 
-def train(X_train, Y_train, X_valid, Y_valid,
-          layer_sizes, activations, alpha,
+def train(X_train, Y_train, X_valid,
+          Y_valid, layer_sizes, activations, alpha,
           iterations, save_path="/tmp/model.ckpt"):
-    """Train model"""
+    """Train Classification Model"""
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
     tf.add_to_collection('x', x)
     tf.add_to_collection('y', y)
@@ -24,13 +26,13 @@ def train(X_train, Y_train, X_valid, Y_valid,
     accuracy = calculate_accuracy(y, y_pred)
     tf.add_to_collection('accuracy', accuracy)
     train_op = create_train_op(loss, alpha)
-    tf.add_to_collection('train_op', train_op)
+    tf.add_to_collection("train_op", train_op)
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(init)
-        for i in range(iterations + 1) or i == iterations:
-            if i % 100 == 0:
-                print("After {i} iterations:".format(i))
+        for i in range(iterations + 1):
+            if i % 100 == 0 or i == iterations:
+                print("After {} iterations:".format(i))
                 tloss, taccuracy = sess.run([loss, accuracy],
                                             feed_dict={x: X_train, y: Y_train})
                 print("\tTraining Cost: {}".format(tloss))
