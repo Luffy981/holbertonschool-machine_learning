b0fc9bcb28ffd978b397fec8993984a08eaec607
Luffy981 <jhonsmith981@hotmail.com>
2022-05-24 01:34:49 -0500

    specificity of confusion matrix

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -1,33 +1,29 @@
 #!/usr/bin/env python3
 """
-    Module contains
-    specificity(confusion):
+specificity for each class in confusion matrix
 """
-
-
 import numpy as np
-
 
 def specificity(confusion):
     """
-        Calculates the specificity for each class in
-          a confusion matrix
-        Args:
-          confusion: numpy.ndarray (classes, classes) - where row
-            indices represent the correct labels and column indices
-            represent the predicted labels.
-        Returns:
-          numpy.ndarray (classes,) - containing the specificity
-            of each class.
+    specificity for each class in a confusion matrix
+    Args:
+        confusion: (classes, classes)-where row indices represent the
+        correct labels and column indices represent the predicted labels
+            classes: is the number of classes
+    Returns:
+        (classes,)-containing the precision of each class
     """
+    # ndarray.flatten(order='C')
+    # Return a copy of the array collapsed into one dimension.
     cSum = np.sum(confusion.flatten())
+    # scpecificity = true negatives / true negatives + false positives
     specificity = np.zeros((confusion.shape[0]))
-    for x in range(confusion.shape[0]):
-        t_neg = cSum - (
-            np.sum(confusion[x]) + np.sum(confusion[:, x]) -
-            confusion[x][x]
-            )
-        specificity[x] = t_neg/(
-          np.sum(confusion[:, x]) - confusion[x][x] + t_neg
-          )
+    for x in range(confusion.shape[0]): 
+        true_negative = cSum - (np.sum(confusion[x]) + np.sum(confusion[:, x])
+                                 - confusion[x][x])
+        specificity[x] = true_negative / (true_negative +
+         (np.sum(confusion[:, x]) - confusion[x][x]))
+    
     return specificity
+
