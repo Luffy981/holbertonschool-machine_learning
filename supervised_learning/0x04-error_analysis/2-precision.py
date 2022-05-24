3574d47c7556261cdfc3c8e2f11980bb332844a7
Luffy981 <jhonsmith981@hotmail.com>
2022-05-23 23:40:36 -0500

    Precision of confusion matriz

---
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Precision for confusion matrix
+"""
+
+
+def precision(confusion):
+    """
+    precision for each class in a confusion matrix
+    Args:
+        confusion: (classes, classes)-where row indices represent the
+        correct labels and column indices represent the predicted labels
+            classes: is the number of classes
+    Returns:
+        (classes,)-containing the precision of each class
+    """
+    precision = np.zeros((confusion.shape[0]))
+    for x in range(confusion.shape[0]):
+        # confusion[x][x] - true Positive
+        # True positive + False positive = Column confusion
+        precision[x] = confusion[x][x]/np.sum(confusion[:, x])
+    return precision
