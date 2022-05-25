5d8a1d48e8decae703077686c7ce72e64c31d1e3
Luffy981 <jhonsmith981@hotmail.com>
2022-05-25 11:30:05 -0500

    evaluate model

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -6,19 +6,17 @@ import tensorflow.compat.v1 as tf
 
 
 def evaluate(X, Y, save_path):
-    """Evaluation method for classification model"""
+    """
+    Evaluation method
+    for classification model
+    """
     with tf.Session() as sess:
         saver = tf.train.import_meta_graph(save_path + '.meta')
         saver.restore(sess, save_path)
-
         x = tf.get_collection('x')[0]
-        y = tf.get_collection('y')[0]
-        y_pred = tf.get_collection('y_pred')[0]
-        accuracy = tf.get_collection('accuracy')[0]
-        loss = tf.get_collection('loss')[0]
-
-        prediction = sess.run(y_pred, feed_dict={x: X, y: Y})
-        accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
-        loss = sess.run(loss, feed_dict={x: X, y: Y})
-
-        return prediction[0], accuracy[0], loss[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        loss = tf.get_collection("loss")[0]
+        acc = tf.get_collection("accuracy")[0]
+        vars = sess.run([y_pred, acc, loss], feed_dict={x: X, y: Y})
+        return vars[0], vars[1], vars[2]
