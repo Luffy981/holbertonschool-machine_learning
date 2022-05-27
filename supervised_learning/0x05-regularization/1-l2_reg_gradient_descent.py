b9fd11fe8fbd4dac45d4b7361a4708c6a5db6624
Luffy981 <jhonsmith981@hotmail.com>
2022-05-26 23:03:27 -0500

    Gradient Descent with L2 Regularization

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -21,24 +21,19 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """
     m = Y.shape[1]
     cweights = weights.copy()
-    for i in range(L, 1, -1):
+    for i in range(L, 0, -1):
         if i == L:
             # Current layer error, derivate cost respect to Z
             curr_layer_err = cache['A'+str(i)] - Y
-            # derivate cost respect to weight last layer
-            derv_cost_w = np.dot(curr_layer_err, cache['A' + str(i-1)].T) / m
-            # Regularization wih L2
-            dw_L2 = derv_cost_w + (lambtha / m) * cweights['W'+str(i)]
-            derv_cost_b = np.sum(curr_layer_err, axis=1, keepdims=True) / m
         else:
             factor = np.dot(cweights['W'+str(i+1)].T, prev_layer_err)
             # Current layer error
             curr_layer_err = factor * derv_tanh(cache['A' + str(i)])
-            # derivate cost respect
-            derv_cost_w = np.dot(curr_layer_err, cache['A' + str(i-1)].T) / m
-            # Regularization with L2
-            dw_L2 = derv_cost_w + (lambtha / m) * cweights['W'+str(i)]
-            derv_cost_b = np.sum(curr_layer_err, axis=1, keepdims=True) / m
+        # derivate cost respect to weight
+        derv_cost_w = np.dot(curr_layer_err, cache['A' + str(i-1)].T) / m
+        # Regularization with L2
+        dw_L2 = derv_cost_w + ((lambtha / m) * cweights['W'+str(i)])
+        derv_cost_b = np.sum(curr_layer_err, axis=1, keepdims=True) / m
         # Update weights and bias
         weights['W'+str(i)] = cweights['W'+str(i)] - alpha * dw_L2
         weights['b'+str(i)] = cweights['b'+str(i)] - alpha * derv_cost_b
