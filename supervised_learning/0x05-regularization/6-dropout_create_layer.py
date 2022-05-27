af20ea88c9a7f178fadfeb8da6a951181db96b11
Luffy981 <jhonsmith981@hotmail.com>
2022-05-27 00:04:56 -0500

    Create a Layer with Dropout

--- supervised_learning/0x05-regularization/6-dropout_create_layer.py
+++ supervised_learning/0x05-regularization/6-dropout_create_layer.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 """Create a Layer with Dropout"""
-import numpy as np
 import tensorflow.compat.v1 as tf
 
 
 def dropout_create_layer(prev, n, activation, keep_prob):
     """
-    prev is a tensor containing the output of the previous layer
-    n is the number of nodes the new layer should contain
-    activation is the activation function that should be used on the layer
-    keep_prob is the probability that a node will be kept
+    Dropout with tensorflow
+    Args:
+        prev: is a tensor containing the output of the previous layer
+        n: is the number of nodes the new layer should contain
+        activation: is the activation function that should be used on the layer
+        keep_prob: is the probability that a node will be kept
+    Return:
+        the output of the new layer
     """
 
     initialice = tf.keras.initializers.VarianceScaling(mode='fan_avg')
