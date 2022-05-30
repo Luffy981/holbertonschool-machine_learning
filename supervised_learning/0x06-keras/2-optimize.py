32608a4de7bba7d131304c5dbe4a973b4252f201
Luffy981 <jhonsmith981@hotmail.com>
2022-05-30 18:49:03 -0500

    optimize model with adam

---
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""sets up Adam optimization for a keras model"""
+import tensOrfkeras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """
+    Args:
+        network is the model to optimize
+        alpha is the learning rate
+    beta1 is the first Adam optimization parameter
+    beta2 is the second Adam optimization parameter
+
+    """
+    adam_optimizer = K.optimizers.Adam(learning_rate=alpha,
+                                       beta_1=beta1,
+                                       beta_2=beta2)
+    network.compile(loss='categorical_crossentropy',
+                    optimizer=adam_optimizer,
+                    metrics=["accuracy"])
+    return None
