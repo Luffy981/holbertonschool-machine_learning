7b41ef2446ab26bafd6fa5df3690ddde0d17d18e
Luffy981 <jhonsmith981@hotmail.com>
2022-05-31 01:11:02 -0500

    Save a Load model config JSON

---
+++ supervised_learning/0x06-keras/11-config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Model To JSON
+Load from JSON
+"""
+import tensorflow.keras as K
+
+
+def save_config(network, filename):
+    """
+    save model to JSON file
+    """
+    model = network.to_json()
+    with open(filename, 'w') as f:
+        f.write(model)
+
+
+def load_config(filename):
+    """
+    Load from JSON file
+    """
+    with open(filename, 'r') as f:
+        model = f.read()
+    return K.model_from_json(model)
