ca251dad67a8fc7f74860ff2c676fb63bbd2fee9
Luffy981 <jhonsmith981@hotmail.com>
2022-06-04 14:35:11 -0500

    Convolution with Channels

--- math/0x04-convolutions_and_pooling/4-convolve_channels.py
+++ math/0x04-convolutions_and_pooling/4-convolve_channels.py
@@ -10,6 +10,7 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
             m: is the number of images
             h: is the height in pixels of the images
             w: is the width in pixels of the images
+            c: is the number of channels in the image
         kernel: (kh,kw) containing the kernel for the convolution
             kh: is the height of the kernel
             kw: is the width of the kernel
@@ -19,26 +20,31 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     m = images.shape[0]
     h = images.shape[1]
     w = images.shape[2]
+    c = images.shape[3]
     kh = kernel.shape[0]
     kw = kernel.shape[1]
+    kc = kernel.shape[2]
     sh, sw = stride
     # Calculatin output shape
     if padding == 'same':
-        W_out = w
-        H_out = h
         p_h = ((((h - 1) * sh) + kh - h) // 2) + 1
         p_w = ((((w - 1) * sw) + kw - w) // 2) + 1
     elif padding == 'valid':
         p_h = 0
         p_w = 0
-    W_out = (w - kw + (2 * p_w)) // sw + 1
-    H_out = (h - kh + (2 * p_h)) // sh + 1
-    p_images = np.pad(images, ((0, 0), (p_h, p_h), (p_w, p_w)), 'constant')
-    output_matriz = np.zeros((m, H_out, W_out))
+    elif type(padding) == tuple:
+        p_h, p_w = padding
+        W_out = (w - kw + (2 * p_w)) // sw + 1
+        H_out = (h - kh + (2 * p_h)) // sh + 1
+        p_images = np.pad(images, ((0, 0),
+                                   (p_h, p_h),
+                                   (p_w, p_w),
+                                   (0, 0)),
+                          'constant')
+        output_matriz = np.zeros((m, H_out, W_out))
     for i in range(W_out):
         for j in range(H_out):
             # np.tensordot(a2D,a3D,((-1,),(-1,))).transpose(1,0,2)
-            # o[:,j,i]= (kernel * images[:,j:j+kh, i:i+kw]).sum(axis=(1,2))
             part_image = p_images[:, sh*j:sh*j + kh, sw*i:sw*i + kw]
-            output_matriz[:, j, i] = np.tensordot(part_image, kernel, axes=2)
+            output_matriz[:, j, i] = np.tensordot(part_image, kernel, axes=3)
     return output_matriz
