d6229ad6670fe1e33c1f857f6d589485915056f8
Luffy981 <jhonsmith981@hotmail.com>
2022-06-10 17:29:26 -0500

    forward propagation in CNN

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -2,7 +2,7 @@
 """
 Forward propagation in CNN
 """
-import numpy  as np
+import numpy as np
 
 
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
@@ -38,18 +38,22 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     elif padding == 'valid':
         p_h, p_w = 0, 0
     elif type(padding) == tuple:
-        p_h, p_w = padding    
+        p_h, p_w = padding
     p_images = np.pad(A_prev, ((0, 0),
                                (p_h, p_h),
                                (p_w, p_w),
                                (0, 0)),
                       'constant')
-    h_out = ((h_prev - kh + 2 * p_h) // sh) + 1 
+    h_out = ((h_prev - kh + 2 * p_h) // sh) + 1
     w_out = ((w_prev - kw + 2 * p_w) // sw) + 1
     conv = np.zeros((m, h_out, w_out, c_new))
     for k in range(c_new):
         for h in range(h_out):
             for w in range(w_out):
                 part_image = p_images[:, sh*h:sh*h+kh, sw*w:sw*w+kw]
-                conv[:, h, w, k] = np.tensordot(part_image, W[:, :, :, k], axes=3) + b[:, :, :, k]
+                conv[:, h, w, k] = np.tensordot(part_image,
+                                                W[:, :, :, k], axes=3) + b[:,
+                                                                           :,
+                                                                           :,
+                                                                           k]
     return activation(conv)
