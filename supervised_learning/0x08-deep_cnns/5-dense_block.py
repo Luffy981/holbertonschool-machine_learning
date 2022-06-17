61731bdee1db84c8dbc1f116c54b648c3595429f
Luffy981 <jhonsmith981@hotmail.com>
2022-06-17 09:39:41 -0500

    dense block

--- supervised_learning/0x08-deep_cnns/5-dense_block.py
+++ supervised_learning/0x08-deep_cnns/5-dense_block.py
@@ -11,13 +11,13 @@ def dense_block(X, nb_filters, growth_rate, layers):
     """
     initializer = K.initializers.HeNormal()
     for layer in range(layers):
-        batch1 = K.layers.BatchNormalization()(X)
+        batch1 = K.layers.BatchNormalization(axis=3)(X)
         activation1 = K.layers.Activation('relu')(batch1)
         conv1 = K.layers.Conv2D(filters=(4 * growth_rate),
                                 kernel_size=(1, 1),
                                 padding='same',
                                 kernel_initializer=initializer)(activation1)
-        batch2 = K.layers.BatchNormalization()(conv1)
+        batch2 = K.layers.BatchNormalization(axis=3)(conv1)
         activation2 = K.layers.Activation('relu')(batch2)
         conv2 = K.layers.Conv2D(filters=growth_rate,
                                 kernel_size=(3, 3),
