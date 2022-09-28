54bbba034c9506a3b9c95fb943edc6fc622452b0
Luffy <jhonsmith981@hotmail.com>
2022-09-28 17:14:27 -0500

    decoder RNN

---
+++ supervised_learning/0x11-attention/2-rnn_decoder.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Create a class RNNDecoder to decode for machine translation
+"""
+
+import tensorflow as tf
+SelfAttention = __import__('1-self_attention').SelfAttention
+
+
+class RNNDecoder(tf.keras.layers.Layer):
+    """
+    Class to decode for machine translation
+    """
+    def __init__(self, vocab, embedding, units, batch):
+        """
+        Class constructor
+        """
+        super(RNNDecoder, self).__init__()
+        self.embedding = tf.keras.layers.Embedding(input_dim=vocab,
+                output_dim=embedding)
+        self.gru = tf.keras.layers.GRU(units=units,
+                return_state=True,
+                return_sequences=True,
+                recurrent_initializer="glorot_uniform")
+        self.F = tf.keras.layers.Dense(units=vocab)
+
+    def call(self, x, s_prev, hidden_states):
+        """
+        x is a tensor of shape (batch, 1) containing the previous word in the
+        target sequence as an index of the target vocabulary
+        s_prev is a tensor of shape (batch, units) containing the previous
+        decoder hidden state
+        hidden_states is a tensor of shape (batch, input_seq_len, units)
+        containing the outputs of the encoder
+        You should concatenate the context vector with x in that order
+        Returns: y, s
+            y is a tensor of shape (batch, vocab) containing the output word
+            as a one hot vector in the target vocabulary
+            s is a tensor of shape (batch, units) containing the new decoder
+            hidden state
+        """
+        units = s_prev.get_shape().as_list()[1]
+        attention = SelfAttention(units)
+        context, weights = attention(s_prev, hidden_states)
+        x = self.embedding(x)
+        context = tf.expand_dims(context, 1)
+        x = tf.concat([context, x], axis=-1)
+        y, s = self.gru(x)
+        y = tf.reshape(y, (-1, y.shape[2]))
+        y = self.F(y)
+        return y, s
