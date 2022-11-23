04fbb80c32eeb2aae6c57c182d0779192396d751
Jhon Smith Flores <jhonsmith981@hotmail.com>
2022-11-22 22:00:44 -0500

    indexing

---
+++ pipeline/0x00-pandas/10-index.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Script to index the pd.DataFrame on the Timestamp column
+"""
+
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+df = df.set_index('Timestamp')
+
+print(df.tail())
