4b39f9e24a41dacf584f3b0f65cbaa3206b88be8
Jhon Smith Flores <jhonsmith981@hotmail.com>
2022-11-22 22:01:45 -0500

    analize

---
+++ pipeline/0x00-pandas/13-analyze.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Script to calculate descriptive statistics for all columns in pd.DataFrame
+except Timestamp
+"""
+
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+stats = df.drop(columns=['Timestamp']).describe()
+
+print(stats)
