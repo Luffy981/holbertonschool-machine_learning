cbb85e9758ff29c87aef2da4fe45bc9d6974b5d2
Jhon Smith Flores <jhonsmith981@hotmail.com>
2022-11-22 22:00:15 -0500

    prune

---
+++ pipeline/0x00-pandas/8-prune.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Script to remove the entries in the pd.DataFrame where Close is NaN
+"""
+
+
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+
+
+df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+df = df[df['Close'].notna()]
+
+print(df.head())
