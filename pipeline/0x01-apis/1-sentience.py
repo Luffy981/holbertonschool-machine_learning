9c11d7a53b5c975487e80ea0793447014be50844
Jhon Smith Flores <jhonsmith981@hotmail.com>
2022-12-01 20:54:56 -0500

    sentience

---
+++ pipeline/0x01-apis/1-sentience.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Method to ping the Star Wars API and return the list of home planets
+for all sentient species
+"""
+
+
+import requests
+
+
+def sentientPlanets():
+    """
+    Returns the list of home planets
+    for all sentient species
+    """
+    url = "https://swapi-api.hbtn.io/api/species/?format=json"
+    speciesList = []
+    while url:
+        results = requests.get(url).json()
+        speciesList += results.get('results')
+        url = results.get('next')
+    homePlanets = []
+    for species in speciesList:
+        if species.get('designation') == 'sentient' or \
+           species.get('classification') == 'sentient':
+            url = species.get('homeworld')
+            if url:
+                planet = requests.get(url).json()
+                homePlanets.append(planet.get('name'))
+    return homePlanets
